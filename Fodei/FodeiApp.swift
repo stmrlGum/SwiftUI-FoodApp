10fca851b57c9e6d680de559e70424f1bf1abb8a
Danil <stmrlgum@gmail.com>
2026-03-11 12:55:44 +1000

    Initial Commit

---
+++ Fodei/FodeiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FodeiApp.swift
+//  Fodei
+//
+//  Created by Danil on 11.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct FodeiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
